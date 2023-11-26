0c5af8707642b2f94c0e53916cf91036a32a5346
Mmackz <matthewmackenz@gmail.com>
2023-11-26 13:56:12 -0800

    test: add tests using mock data

---
+++ test/data/mockdata.js
@@ -0,0 +1,26 @@
+export const mockTokenList = {
+   name: "Invalid Token List",
+   timestamp: "2023-11-11T11:11:11.111Z",
+   version: {
+      major: 1,
+      minor: 0,
+      patch: 0
+   },
+   tokens: [
+      {
+         chainId: 1,
+         address: "0x4200000000000000000000000000000000006969",
+         name: "Valid Token",
+         symbol: "VAL",
+         decimals: 18
+      }
+   ]
+};
+
+export const mockToken = {
+   chainId: 1,
+   address: "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9",
+   name: "Aave",
+   symbol: "AAVE",
+   decimals: 18
+};
