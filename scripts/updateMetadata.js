70a6da6e2dd61657055e99753915063a2e112831
Mmackz <matthewmackenz@gmail.com>
2023-11-26 14:05:07 -0800

    feat: add script to update metadata

---
+++ scripts/updateMetadata.js
@@ -0,0 +1,36 @@
+import {
+   getTokenListsFromFiles,
+   getTokenListFromGit,
+   getTimestamp,
+   getNextVersion
+} from "../src/utils/index.js";
+import fs from "fs";
+import { validate } from "../src/index.js";
+import { isVersionUpdate, versionComparator } from "@uniswap/token-lists";
+
+(async function updateMetaData() {
+   let [update, base] = getTokenListsFromFiles();
+
+   if (!base) {
+      base = await getTokenListFromGit();
+   }
+
+   if (validate(update)) {
+      const newVersion = getNextVersion(base, update);
+      if (
+         isVersionUpdate(base.version, newVersion) &&
+         versionComparator(update.version, newVersion)
+      ) {
+         const timestamp = getTimestamp();
+         update.timestamp = timestamp;
+         update.version = newVersion;
+         fs.writeFileSync("tokenlist.json", JSON.stringify(update, null, 2));
+         console.log("new version: ", newVersion);
+         console.log("metadata updated");
+      } else {
+         console.log("version is up-to-date");
+      }
+   } else {
+      console.log("token list is not valid");
+   }
+})();
